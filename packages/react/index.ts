4f1ca0f2f37f9c47607f63a7c0ca149ef139f3e8
菟丝子 <liuyang@igancao.com>
2023-02-16 17:34:04 +0800

    fix: dev环境打包

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,1 +1,5 @@
-export default {};
+import { jsx } from './src/jsx';
+export default {
+	version: '1.0.0',
+	createElement: jsx
+};
--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -24,3 +24,41 @@ const ReactElement = function (
 	};
 	return element;
 };
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	let key: Key = '';
+	let ref: Ref = null;
+	const props: Props = null;
+
+	for (const prop in config) {
+		const value = config[prop];
+
+		if (prop === 'key') {
+			if (value !== undefined) {
+				key = '' + value;
+			}
+		}
+		if (prop === 'ref') {
+			if (value !== undefined) {
+				ref = '' + value;
+			}
+		}
+
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = value;
+		}
+
+		const maybeChildrenLength = maybeChildren.lengh;
+		if (maybeChildrenLength) {
+			if (maybeChildrenLength === 1) {
+				props.children = maybeChildren[0];
+			} else {
+				props.children = maybeChildren;
+			}
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = jsx;
