8e54511c957e29b52845ab5f030a7d45b4c05945
菟丝子 <liuyang@igancao.com>
2023-02-24 18:57:13 +0800

    feat: reconciler

--- packages/react-reconciler/src/fiber.ts
+++ packages/react-reconciler/src/fiber.ts
@@ -1,10 +1,10 @@
 import { Props, Key } from 'shared/ReactTypes';
 import { WorkTag } from './workTags';
 export class FiberNode {
-    constructor(tag: WorkTag, pendingProps: Props, key: Key) {
-        // pendingProps fiberNode接下来有哪些状态要改变
-        //key ReactELmentKey
+	constructor(tag: WorkTag, pendingProps: Props, key: Key) {
+		// pendingProps fiberNode接下来有哪些状态要改变
+		//key ReactELmentKey
 
-        this.tag = tag; //实例属性
-    }
+		this.tag = tag; //实例属性
+	}
 }
