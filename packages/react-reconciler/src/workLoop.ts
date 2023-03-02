6641b47d87aac5f34d113275c5c7de9cfb71adb8
菟丝子 <liuyang@igancao.com>
2023-03-02 17:55:10 +0800

    feat: reconciler

--- packages/react-reconciler/src/workLoop.ts
+++ packages/react-reconciler/src/workLoop.ts
@@ -3,49 +3,49 @@ import { FiberNode } from './fiber';
 let workInProgress: FiberNode | null = null;
 
 function prepareFreshStack(fiber: FiberNode) {
-    workInProgress = fiber;
+	workInProgress = fiber;
 } //让当前的workInprogress 指向要执行的第一个fiberNode
 
 function renderRoot(root: FiberNode) {
-    prepareFreshStack(root);
+	prepareFreshStack(root);
 
-    do {
-        try {
-            workLoop();
-            break;
-        } catch (error) {
-            workInProgress = null;
-        }
-    } while (true);
+	do {
+		try {
+			workLoop();
+			break;
+		} catch (error) {
+			workInProgress = null;
+		}
+	} while (true);
 } //最终执行的方法
 function workLoop() {
-    while (workInProgress !== null) {
-        perFormUnitOfWork(workInProgress);
-    }
+	while (workInProgress !== null) {
+		perFormUnitOfWork(workInProgress);
+	}
 }
 function perFormUnitOfWork(fiber: FiberNode) {
-    const next = beginWork(fiber);
-    fiber.memoizedProps = fiber.pendingProps;
-    if (next === null) {
-        completeUnitOfWork(fiber);
-    } else {
-        workInProgress = next;
-    }
+	const next = beginWork(fiber);
+	fiber.memoizedProps = fiber.pendingProps;
+	if (next === null) {
+		completeUnitOfWork(fiber);
+	} else {
+		workInProgress = next;
+	}
 }
 
 function completeUnitOfWork(fiber: FiberNode) {
-    let node: FiberNode | null = fiber;
+	let node: FiberNode | null = fiber;
 
-    do {
-        completeWork(node);
+	do {
+		completeWork(node);
 
-        const sibling = node.sibling;
-        if (sibling !== null) {
-            workInProgress = node.sibling;
-            return;
-        }
+		const sibling = node.sibling;
+		if (sibling !== null) {
+			workInProgress = node.sibling;
+			return;
+		}
 
-        node = node.return;
-        workInProgress = node;
-    } while (node !== null);
+		node = node.return;
+		workInProgress = node;
+	} while (node !== null);
 }
