399700210960abfda03b8415364125232d7ac92f
菟丝子 <liuyang@igancao.com>
2023-03-18 19:28:08 +0800

    feat: fiber实现

--- packages/react-reconciler/src/fiberReconciler.ts
+++ packages/react-reconciler/src/fiberReconciler.ts
@@ -2,31 +2,31 @@ import { Container } from 'hostConfig';
 import { FiberNode, FiberRootNode } from './fiber';
 import { HostRoot } from './workTags';
 import {
-    UpdateQueue,
-    createUpdate,
-    createUpdateQueue,
-    enqueueUpdate
+	UpdateQueue,
+	createUpdate,
+	createUpdateQueue,
+	enqueueUpdate
 } from './updateQueue';
 import { ReactElment } from 'shared/ReactTypes';
 
 export function createContainer(container: Container) {
-    const hostRootFiber = new FiberNode(HostRoot, {}, null);
-    const root = new FiberRootNode(container, hostRootFiber);
+	const hostRootFiber = new FiberNode(HostRoot, {}, null);
+	const root = new FiberRootNode(container, hostRootFiber);
 
-    hostRootFiber.updateQueue = createUpdateQueue();
-    return root;
+	hostRootFiber.updateQueue = createUpdateQueue();
+	return root;
 }
 export function updateContainer(
-    element: ReactElment | null,
-    root: FiberRootNode
+	element: ReactElment | null,
+	root: FiberRootNode
 ) {
-    const hostRootFiber = root.current;
-    const update = createUpdate<ReactElment | null>(element);
-    enqueueUpdate(
-        hostRootFiber.updateQueue as UpdateQueue<ReactElment | null>,
-        update
-    );
+	const hostRootFiber = root.current;
+	const update = createUpdate<ReactElment | null>(element);
+	enqueueUpdate(
+		hostRootFiber.updateQueue as UpdateQueue<ReactElment | null>,
+		update
+	);
 
-    scheduleUpdateOnFiber(hostRootFiber);
-    return element;
+	scheduleUpdateOnFiber(hostRootFiber);
+	return element;
 }
--- packages/react-reconciler/src/updateQueue.ts
+++ packages/react-reconciler/src/updateQueue.ts
@@ -1,52 +1,52 @@
 import { Action } from 'shared/ReactTypes';
 
 export interface Update<State> {
-    action: Action<State>;
+	action: Action<State>;
 }
 
 export interface UpdateQueue<State> {
-    shared: {
-        pending: Update<State> | null;
-    };
+	shared: {
+		pending: Update<State> | null;
+	};
 }
 
 export const createUpdate = <State>(action: Action<State>): Update<State> => {
-    //创建update实例
-    return {
-        action
-    };
+	//创建update实例
+	return {
+		action
+	};
 };
 
 export const createUpdateQueue = <State>() => {
-    return {
-        shared: {
-            pending: null
-        }
-    } as UpdateQueue<State>;
+	return {
+		shared: {
+			pending: null
+		}
+	} as UpdateQueue<State>;
 };
 
 export const enqueueUpdate = <State>(
-    upDateQueue: UpdateQueue<State>,
-    upDate: Update<State>
+	upDateQueue: UpdateQueue<State>,
+	upDate: Update<State>
 ) => {
-    upDateQueue.shared.pending = upDate;
+	upDateQueue.shared.pending = upDate;
 };
 
 export const processUpdateQueue = <State>(
-    baseState: State, //初始状态
-    pendingUpdate: Update<State> | null //要消费的update
+	baseState: State, //初始状态
+	pendingUpdate: Update<State> | null //要消费的update
 ): { memoizedState: State } => {
-    const result: ReturnType<typeof processUpdateQueue<State>> = {
-        memoizedState: baseState
-    };
-    if (pendingUpdate !== null) {
-        const action = pendingUpdate.action;
-        if (action instanceof Function) {
-            result.memoizedState = action(baseState);
-        } else {
-            result.memoizedState = action;
-        }
-    }
+	const result: ReturnType<typeof processUpdateQueue<State>> = {
+		memoizedState: baseState
+	};
+	if (pendingUpdate !== null) {
+		const action = pendingUpdate.action;
+		if (action instanceof Function) {
+			result.memoizedState = action(baseState);
+		} else {
+			result.memoizedState = action;
+		}
+	}
 
-    return result;
+	return result;
 };
